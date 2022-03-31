5fe001dfe09729a23e32c124570f2623413c3ca6
Kristina Peru <krissy.germanov@gmail.com>
2022-03-31 15:02:14 -0600

    Clean up

--- src/components/Authorpage.js
+++ src/components/Authorpage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import AuthorCard from './AuthorCard'
 
 function Authorpage({ author }) {
@@ -14,21 +13,3 @@ function Authorpage({ author }) {
 }
 
 export default Authorpage
-
-// function Authorpage({ author }) {
-//   const authorItem = author.map((auth) => (
-//       <div key={auth.id}>
-//           {auth.first_name} {auth.last_name}
-//       </div>
-//   ))
-
-// return (
-// <div class="card w-75">
-//   <div class="card-body">
-//     <h5 class="card-title">{authorItem}</h5>
-//     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-//     <a href="#" class="btn btn-primary">Button</a>
-//   </div>
-// </div>
-// )
-// }
