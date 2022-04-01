beb702c2f02a97010a433a377fd6dbd430fd33d2
Kristina Peru <krissy.germanov@gmail.com>
2022-04-01 08:31:09 -0600

    styling

--- src/components/formpage.js
+++ src/components/formpage.js
@@ -40,96 +40,114 @@ function Formpage({ books, setBooks }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input 
-          type="text"
-          title="title"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </label>
-      <label>
-        Author:
-        <input 
-          type="text"
-          author="author"
-          placeholder="Author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-      </label>
-      <label>
-        Genre:
-        <input 
-          type="text"
-          genre="genre"
-          placeholder="Genre"
-          value={genre}
-          onChange={(e) => setGenre(e.target.value)}
-        />
-      </label>
-      <label>
-        Length:
-        <input 
-          type="number"
-          length="length"
-          placeholder="How many pages..."
-          value={length}
-          onChange={(e) => setLength(e.target.value)}
-        />
-      </label>
-      <label>
-        Reading Time:
-        <input 
-          type="number"
-          reading="reading"
-          placeholder="day(s)"
-          value={reading}
-          onChange={(e) => setReading(e.target.value)}
-        />
-      </label>
-      <label>
-        Image URL:
-        <input 
-          type="text"
-          image="image"
-          placeholder="Image URL"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
-      </label>
-      <label>
-        Description:
-        <textarea
-          description="description"
-          value={description}
-          placeholder="Book summary"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </label>
-      <label>
-        Rating:
-        <input 
-          type="number"
-          rating="rating"
-          placeholder="1-5"
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
-        />
-      </label>
-      <label>
-        Comment/Review:
-        <textarea
-          comment="comment"
-          placeholder="What did you think?"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-      </label>
-      <button type="submit">Add to Books</button>
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="tit">
+        <label for="exampleFormControlInput1" className="form-label">
+          Title:
+          <input className="input"
+            type="text"
+            title="title"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="aut">
+        <label for="exampleFormControlInput1" className="form-label">
+          Author:
+          <input 
+            type="text"
+            author="author"
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="gen">
+        <label for="exampleFormControlInput1" className="form-label">
+          Genre:
+          <input 
+            type="text"
+            genre="genre"
+            placeholder="Genre"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="len">
+        <label for="exampleFormControlInput1" className="form-label">
+          Length:
+          <input 
+            type="number"
+            length="length"
+            placeholder="How many pages..."
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="tim">
+        <label for="exampleFormControlInput1" className="form-label">
+          Reading Time:
+          <input 
+            type="number"
+            reading="reading"
+            placeholder="day(s)"
+            value={reading}
+            onChange={(e) => setReading(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="rat">
+        <label for="exampleFormControlInput1" className="form-label">
+          Rating:
+          <input 
+            type="number"
+            rating="rating"
+            placeholder="1-5"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="ima">
+        <label className="form-label">
+          Image URL:
+          <input 
+            type="text"
+            image="image"
+            placeholder="Image URL"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="des">
+        <label className="form-label">
+          Description: 
+          <textarea
+            description="description"
+            value={description}
+            placeholder="Book summary"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="com">
+        <label className="form-label">
+          Comment/Review: 
+          <textarea
+            comment="comment"
+            placeholder="What did you think?"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </label>
+      </div>
+      <button className="btn btn-outline-light btn-lg" type="submit">Add to Books</button>
     </form>
   )
 }
